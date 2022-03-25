c3989e6d8447159d4f929e6d1082656e909380c3
arif99050 <arifulislam99050@gmail.com>
2022-03-25 23:15:35 +0600

    show watch name at cart(orders) section

--- src/Component/Shop/Shop.js
+++ src/Component/Shop/Shop.js
@@ -4,12 +4,20 @@ import './Shop.css'
 
 const Shop = () => {
     const [watches, setWatches]=useState([])
+    const [cart,setCart]=useState([])
     useEffect( ()=>{
         fetch('watch.json')
         .then(res=>res.json())
         .then(data=>setWatches(data))
     },[])
     
+    const handleAddToCart=(watch)=>{
+        const newcart=[...cart,watch]
+        
+        setCart(newcart);
+        console.log(cart)
+
+    }
     
     return (
 
@@ -21,12 +29,16 @@ const Shop = () => {
                     watches.map(watch=> <Watch
                     watch={watch}
                     key={watch.id}
+                    handleAddToCart={handleAddToCart}
                     ></Watch>)
                 }
                 
             </div>
             <div className='cart'>
                 <h3>Your Orders</h3>
+                {
+                    cart.map(c=> <h4>{c.name}</h4>)
+                }
 
             </div>
 
--- src/Component/Watch/Watch.js
+++ src/Component/Watch/Watch.js
@@ -1,16 +1,21 @@
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import { faShoppingCart } from '@fortawesome/free-solid-svg-icons';
 import React from 'react';
 import './Watch.css'
 
+
 const Watch = (props) => {
     const{name,price,img}=props.watch
+    const {handleAddToCart}=props;
     return (
         <div className='watchDetails'>
              <img className='watchImage' src={img} alt=""/>
             <h1 className='watchName'>Name : {name}</h1>
            
             <h4 className='watchPrice'>Price: {price}</h4>
-            <button className='addToCartBtn'>
-                <p>Add to cart</p>
+            <button onClick={()=>{handleAddToCart(props.watch)}} className='addToCartBtn'>
+                <p className='btn-text'>Add to cart</p>
+                <span> <FontAwesomeIcon icon={faShoppingCart}></FontAwesomeIcon> </span>
             </button>
             
         </div>
