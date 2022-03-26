485f56e129983e342363a34e66a526dcdc941850
arif99050 <arifulislam99050@gmail.com>
2022-03-26 21:25:35 +0600

    handle reset option

--- src/Component/Shop/Shop.js
+++ src/Component/Shop/Shop.js
@@ -20,8 +20,9 @@ const Shop = () => {
     }, [])
 
     const handleAddToCart = (watch) => {
-        const newcart = [...cart, watch]
         
+     const newcart = [...cart, watch]
+       
         setCart(newcart);
     }
     const handleRandom=()=>{
@@ -33,9 +34,11 @@ const Shop = () => {
     }
     
    
-  const resetCart=document.getElementById('cart')
+  
   const reset=()=>{
-      resetCart.innerHTML='';
+      //resetCart.innerHTML='';
+       setCart([])
+       setRandom([])
   }
 
 
@@ -53,6 +56,7 @@ const Shop = () => {
                            
                         ></Watch>)
                     }
+                    
                     
 
                 </div>
@@ -90,6 +94,19 @@ const Shop = () => {
                 </div>
 
             </div>
+            <h1>How react works?</h1>
+            <p>
+                Answer: React is a javascript library and it uses for building usuer interface. React basically maintains a tree and this tree is able to do efficient diff computations on the nodes. In react every compents return JSX and its syntex is similiar to html but not real html. We can write javascript code with {} within jsx. React main virtual dom and when we want to change dom element ,react just rerender the elements which we want to change, react don't rerender the whole dom. For this website run faster.
+            </p>
+            <h1>State vs Props</h1>
+                    <p>Answe :In state We can define state in the component itself , In props we can pass properties from parent component.
+                        State are mututable and props are immutable.
+                        State change can be asynchronus, props are read only.
+                        State can not be accessed by child component, and props can be asccessed by child component.
+                        State can not make component reuseable, props make component reuseable.
+                        State can be modified using setState, props can not be modified.
+                    </p>
+              
 
 
 
